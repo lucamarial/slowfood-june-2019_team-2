28536c63c62ed4eb3f347ce76ce263cf26288e40
Becca <beccaburns89@gmail.com>
2019-10-03 16:28:53 +0200

    Populates DB with seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 starter = Category.create(title: "Starters")
-Product.create(name: "Salad", description: "tomatoes, leafs", price: 10, category: starter )
+Product.create(title: "Salad", content: "tomatoes, leafs", price: 10, category: starter )
 
 
 entree = Category.create(title: "Main Dishes")
-Product.create(name: "Swedish meatballs", description: "ground beef, parsley, gravy", price: 30, category: entree)
-Product.create(name: "Surströmming", description: "herring, misery", price: 30, category: entree)
+Product.create(title: "Swedish meatballs", content: "ground beef, parsley, gravy", price: 30, category: entree)
+Product.create(title: "Surströmming", content: "herring, misery", price: 30, category: entree)
 
 
 dessert = Category.create(title: "Desserts")
-Product.create(name: "Apple pie", description: "apples, cinnamon, sugar", price: 20, category: dessert)
-Product.create(name: "Princess cake", description: "sponge cake, cream, jam", price: 20, category: dessert)
+Product.create(title: "Apple pie", content: "apples, cinnamon, sugar", price: 20, category: dessert)
+Product.create(title: "Princess cake", content: "sponge cake, cream, jam", price: 20, category: dessert)
