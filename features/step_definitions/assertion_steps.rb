73af77762bc8793da167065bcb9ce0215f7315e8
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-03 22:45:13 +0200

    Remove redundant step definition

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -5,7 +5,3 @@ end
 Then("I should see {string} button") do |element|
   expect(page).to have_content element
 end
-
-Then("I should see {string} in the {string} section") do |product, category|
-  pending # Write code here that turns the phrase above into concrete actions
-end
