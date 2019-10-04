c443cc436d03a68f763ffecedeae43287b52e502
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-04 14:04:13 +0200

    Generate and configure Cartify
    Migrate and add path to routes file.
    Create cartify initializer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products, only: [:show]
+  mount Cartify::Engine, at: '/'
   devise_for :users
   resources :products, only: [:index]
   root controller: :products, action: :index
