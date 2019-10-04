46f948dfcb805e3ad1fe5c3904a6a28389272d6f
AlexSaxena <saxenaalex@gmail.com>
2019-10-04 13:12:44 +0200

    removes feature file [WIP]

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_110617) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "rights"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
