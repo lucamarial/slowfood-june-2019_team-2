4e08256746462cfe25e8a531ca113480a921c5ec
AlexSaxena <saxenaalex@gmail.com>
2019-10-05 15:38:34 +0200

    Removes duplicate code in cartify.rb

--- config/initializers/cartify.rb
+++ config/initializers/cartify.rb
@@ -15,9 +15,3 @@ Cartify.title_attribute = :name
 
 # the price of the product
 Cartify.price_attribute = :price 
-
-Cartify.product_class = 'Product'
-Cartify.user_class = 'User'
-Cartify.empty_cart_path = 'root_path'
-Cartify.title_attribute = :name
-Cartify.price_attribute = :price
