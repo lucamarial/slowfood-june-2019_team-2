bc735511e818fa9463a1ea0730247dd5e4312f9e
luca <l.lobacher@web.de>
2019-10-05 16:37:18 +0200

    Changes in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2019_10_04_115129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "cartify_addresses", force: :cascade do |t|
     t.string "type"
     t.string "first_name"
@@ -114,4 +126,5 @@ ActiveRecord::Schema.define(version: 2019_10_04_115129) do
   add_foreign_key "cartify_orders", "cartify_credit_cards", column: "credit_card_id"
   add_foreign_key "cartify_orders", "cartify_deliveries", column: "delivery_id"
   add_foreign_key "cartify_orders", "cartify_order_statuses", column: "order_status_id"
+  add_foreign_key "products", "categories"
 end
