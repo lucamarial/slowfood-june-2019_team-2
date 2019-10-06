0ad6f76955299c63c559cf8bd84c60633928be1f
Becca <beccaburns89@gmail.com>
2019-10-06 16:51:17 +0200

    Updates ability for owner to edit products. Includes edit form and updates index html haml. Updates root for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,17 @@ class ProductsController < ApplicationController
   def index 
     @categories = Category.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:title, :content, :price, :category_id)
+  end  
 end
