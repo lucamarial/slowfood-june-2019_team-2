2c95379ca7e92c6a05a752d4b35e866a11d7c8c6
Becca <beccaburns89@gmail.com>
2019-10-06 15:36:24 +0200

    Includes feature and corresponding tests for owner to edit product list.

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -20,3 +20,34 @@ When("I click {string} on {string}") do |element, product_name|
     click_on element
   end
 end
+
+And ("the following user exists") do |table|
+
+end
+
+And("I am on the landing page") do
+  visit root_path
+end
+
+And("I click {string}") do |string|
+  click_on string
+end
+
+Given("I fill in {string} with {string}") do |string, string2|
+
+end
+
+When("I click {string}") do |string|
+
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+end
+
+When("I choose {string}") do |string|
+
+end
+
+Then("I visit the landing page") do
+
+end
