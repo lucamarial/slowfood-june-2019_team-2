a8b8ff0bceb9720740b73416398b82e872d2e797
Becca <beccaburns89@gmail.com>
2019-10-06 16:53:56 +0200

    Includes Update in product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,16 @@ class ProductsController < ApplicationController
   def edit
     @product = Product.find(params[:id])
   end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to root_path
+    else 
+      render 'edit'
+    end
+  end
   
   private
   def product_params
